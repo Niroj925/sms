71b88b24f71b87957e41388a234685cd3828f7d3
Niroj925 <ngtc0925@gmail.com>
2022-04-18 18:20:18 +0545

    file uploaded

---
+++ app.js
@@ -0,0 +1,65 @@
+const express=require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path= require('path');
+const mongoose = require('mongoose');
+const viewpath = path.join(__dirname, './view');
+
+const client=require('twilio')('ACc495fd51357629b07f63d7e49844f773','c465a0877cb12d2ca5298a63f71a5243');
+
+app.set('view engine', 'ejs');
+app.set('views',viewpath);
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/message");
+
+const msgSchema = {
+    msg:String
+}
+
+const listmsg=mongoose.model('item',msgSchema);
+
+app.get('/',function(req, res){
+    listmsg.findOne({},function(err,founditem){
+           res.render('home',{message:founditem});
+    })
+})
+
+app.post('/',function(req, res){
+    const text=req.body.message;
+    console.log(text);
+   
+
+    const meromsg=new listmsg({
+        msg:text
+    })
+    listmsg.findOne({},function(err,founditem){
+         if(founditem.length===0){
+           meromsg.save();
+           }else{
+        listmsg.updateOne({_id:founditem.id},{$set:{msg:text}},function(err){
+            if(!err){
+                console.log('saved msg');
+            }
+        });
+    }
+  res.redirect('/');
+    })
+
+  //send messages using twilio
+   client.messages.create({
+    body:text,
+    to:'+9779867816329',
+    from:'+16204728692'
+}).then(message =>console.log('message sent'))
+
+.catch(err =>console.log('error'))
+
+})
+
+const port=process.env.PORT||3400;
+
+app.listen(port,function(){
+    console.log('server is running');
+});
